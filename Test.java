36966c46bc19dac3dc37843f57a65e99b50f1584
Dung_BlackW <43365759+nguyentiendungit2912@users.noreply.github.com>
2018-12-12 20:15:38 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+	
+	public static void main(String[] args){
+		MyBigNumber mybignumber = new MyBigNumber();
+		//mybignumber.sum("12","12");
+		System.out.println(mybignumber.sum("1234","987"));
+	}
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+	
+	public static void main(){
+		MyBigNumber mybignumber = new MyBigNumber();
+		mybignumber.sum("12","1");
+	}
+}
